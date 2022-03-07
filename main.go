99b45199d57ef2903005a6a009943ffe090b1788
FreezeOMatic <100123383+FreezeOMatic@users.noreply.github.com>
2022-03-07 21:35:19 +0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"std/io/ioutil"
+	"strconv"
+)
+
+// точка входа
+func main() {
+	botToken := "5239609268:AAGIZYVop_okc0hZU7skgs7OIo1jlKbcxlg"
+	// https://api.telegram.org/bot<token>/METHOD_NAME
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
+	for {
+		updates, err := getUpdates(botUrl, offset)
+		if err != nil {
+			log.Println("smt went wrong", err.Error())
+		}
+		for _, update := range updates {
+			err = respond(botUrl, update)
+			offset = update.UpdateId + 1
+			if err != nil {
+				fmt.Println("тут беда", err)
+			}
+		}
+		fmt.Println(updates)
+	}
+}
+
+// запрос обновлений
+func getUpdates(botUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates" + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(body))
+	var restResponse RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+	return restResponse.Result, nil
+}
+
+// ответ на обновления
+func respond(botUrl string, update Update) error {
+	var botMessage BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = update.Message.Text
+	buf, err := json.Marshal(botMessage)
+	fmt.Println(botUrl + "/sendMessage")
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+	return nil
+}
